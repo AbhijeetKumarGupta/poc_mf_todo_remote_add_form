9af82399b1215deb0434565d10b2350efc55fe44
AbhijeetKumarGupta <abhijeetg375@gmail.com>
2024-03-21 18:07:02 +0530

    Add form skeleton added

---
+++ src/module/AddForm/index.js
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import { AddButton, AddFormContainer, ErrorContainer, InputFieldWrapper, TodoInputField } from "./style";
+
+function AddForm() {
+    const [todoTitle, setTodoTitle] = useState("");
+    const [error, setError] = useState("");
+
+    const handleChange = (e) => {
+        setTodoTitle(e?.target?.value)
+    }
+    const handleAdd = (e) => {
+
+    }
+
+    return (
+      <AddFormContainer onSubmit={()=>{}}>
+          <h3>Add todo form</h3>
+          <InputFieldWrapper>
+            <TodoInputField
+              type="text"
+              value={todoTitle}
+              placeholder="Enter Text Here"
+              onChange={handleChange}
+            />
+            {error && 
+            <ErrorContainer>
+              {
+              error
+              }
+            </ErrorContainer>}
+          </InputFieldWrapper>
+          <AddButton 
+            onClick={handleAdd}
+          >
+            Add
+          </AddButton>
+      </AddFormContainer>
+    );
+  }
+  
+  export default AddForm;
+  
---
+++ src/module/AddForm/style.js
@@ -0,0 +1,42 @@
+import styled from "styled-components";
+
+export const AddFormContainer = styled.form`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin: 0 auto;
+    margin-top: 20px;
+    margin-bottom: 20px;
+    width: 50%;
+    color: white;
+    gap: 15px;
+`
+
+export const InputFieldWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 100%;
+`
+
+export const TodoInputField = styled.input`
+    padding: 10px;
+    width: 50%;
+`
+
+export const ErrorContainer = styled.span`
+    color: red;
+`
+
+export const AddButton = styled.button`
+    text-align: center;
+    padding: 10px;
+    background-color: rgba(144, 238, 144, 0.6) ;
+    border: 2px solid white;
+    margin-left: 5px;
+    width: 10%;
+    min-width: 70px;
+    cursor: pointer;
+    border-radius: 5px;
+    color: white;
+`
